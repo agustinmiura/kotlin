660c5954840e042bab15ccbc4731a7e88e7506e9
Agustin Miura <agustin.miura@gmail.com>
2021-04-04 20:45:52 -0300

    Finished null operator

--- tutorial/src/main/kotlin/ar/com/miura/kotlin/datatypes/NullReferences.kt
+++ tutorial/src/main/kotlin/ar/com/miura/kotlin/datatypes/NullReferences.kt
@@ -2,4 +2,18 @@ package ar.com.miura.kotlin.datatypes
 
 fun main(args: Array<String>) {
 
+    var string = "string"
+    //we can't do this
+    //string = null
+
+    var nullableString:String? = null
+
+    if (nullableString != null) {
+        println(" The variable is : ${nullableString.toUpperCase()} ")
+    }
+    //if it isn't null safe call operator
+    println(nullableString?.toUpperCase())
+
+    val str2: String = nullableString?:"Default value"
+
 }
