c047a68eb629da57fcb4ab5a790c4ac5672269a2
Agustin Miura <agustin.miura@gmail.com>
2021-04-04 20:15:26 -0300

    Added equality KT

--- tutorial/src/main/kotlin/ar/com/miura/kotlin/equality/Equality.kt
+++ tutorial/src/main/kotlin/ar/com/miura/kotlin/equality/Equality.kt
@@ -2,4 +2,31 @@ package ar.com.miura.kotlin.equality
 
 fun main(args: Array<String>) {
 
+    var employee = Employee("Blue", "Ford", 11)
+    var employee2 = Employee("Blue", "Ford", 11);
+    var employee3 = Employee("Blue", "Ford", 12);
+
+    println(" e vs e2 :  ${employee==employee2}")
+    println(" e vs e3 : ${employee2==employee3}")
+    println(" e === e2 : ${employee === employee2 }")
+    println(" e === e3 : ${employee2 === employee3}")
+
+
 }
+
+class Employee(var color:String,var brand: String,var age: Int) {
+
+    override fun equals(obj: Any?):Boolean {
+        /*instance of
+        * */
+        if (obj is Employee) {
+            return color == obj.color && brand == obj.brand && age == obj.age
+
+        } else {
+            return false;
+
+        }
+    }
+}
+
+
