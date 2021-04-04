51c1a5606e988870c763de012de39bd483dd6195
Agustin Miura <agustin.miura@gmail.com>
2021-04-04 18:59:47 -0300

    Added hello world

---
+++ tutorial/src/main/kotlin/ar/com/miura/java/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package ar.com.miura.java.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(" Hello worlld ");
+    }
+}
---
+++ tutorial/src/main/kotlin/ar/com/miura/kotlin/helloworld/HelloWorld.kt
@@ -0,0 +1,5 @@
+package ar.com.miura.kotlin.helloworld
+
+fun main(args: Array<String>) {
+    println("Hello world")
+}
