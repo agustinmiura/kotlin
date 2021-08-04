1ba351158f8614c6143d8cc6288e0a59a56e4da7
agustinmiura <miura.agustin@gmail.com>
2021-08-03 23:00:43 -0300

    Added factorial for big numbers

---
+++ practice/src/test/ar/com/miura/kotlin/FactorialTestCase.kt
@@ -0,0 +1,21 @@
+package ar.com.miura.kotlin
+
+class FactorialTestCase {
+}
+
+class Solution {
+    fun trailingZeroes(n: Int): Int {
+        var factorial = BigInteger.ONE
+        for(i in 2..n) {
+            factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
+        }
+
+        var zeroCount = 0
+        while(factorial.mod(BigInteger.TEN).equals(BigInteger.ZERO)) {
+            factorial = factorial.divide(BigInteger.TEN)
+            zeroCount++
+        }
+
+        return zeroCount
+    }
+}
