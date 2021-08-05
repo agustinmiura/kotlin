228c3e47fa769aabb1929331d8302d6759c79754
agustinmiura <miura.agustin@gmail.com>
2021-08-04 21:01:57 -0300

    Fixed pow

--- practice/src/main/kotlin/ar/com/miura/lc/mypow/Solution.kt
+++ practice/src/main/kotlin/ar/com/miura/lc/mypow/Solution.kt
@@ -19,12 +19,19 @@ class Solution {
     }
 
     private fun subPow(x:Double, n:Int):Double {
-        var max = n-1
-        var acum = 1.0
-        for(i in 0..max) {
-            acum *= x
-        }
-        return acum
+        return recursivePow(x,n)
     }
 
+    private fun recursivePow(x:Double, n:Int):Double {
+        if (n==0) {
+            return 1.0
+        } else {
+            var temp = recursivePow(x,n/2)
+            if (n%2==0) {
+                return temp * temp
+            } else {
+                return temp * temp * x
+            }
+        }
+    }
 }
