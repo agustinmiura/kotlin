6865b5f40a34a6d4a5c72557ad86a254c8c19be5
agustinmiura <miura.agustin@gmail.com>
2021-12-12 19:48:04 -0300

    Added reverse number by bits

---
+++ practice/src/main/kotlin/java/ar/com/miura/bit/ReverseNumber.java
@@ -0,0 +1,17 @@
+package java.ar.com.miura.bit;
+
+public class ReverseNumber {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+
+        int result = 0;
+
+        for(int i=0;i<32;i++) {
+            int currentBit = (n>>>i) & 1;
+            int spaces = 31 - i;
+            result = (result | (currentBit << spaces));
+        }
+        return result;
+
+    }
+}
