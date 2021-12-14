1fbe6b610000bf7ebd7b9aa72404452b7a5744f8
agustinmiura <miura.agustin@gmail.com>
2021-12-13 21:43:18 -0300

    Added nested list 2 recursive

---
+++ practice/src/main/kotlin/java/ar/com/miura/graph/NestedInteger.java
@@ -0,0 +1,14 @@
+package java.ar.com.miura.graph;
+
+public interface NestedInteger {
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public void setInteger(int value);
+
+    public void add(NestedInteger ni);
+
+    public java.util.List<java.ar.com.miura.graph.NestedInteger> getList();
+
+}
