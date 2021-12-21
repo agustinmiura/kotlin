54b95759abb5b5572b35267ccd70d9a919cd4126
agustinmiura <miura.agustin@gmail.com>
2021-12-20 21:01:35 -0300

    Added problem

---
+++ practice/src/main/kotlin/ar/com/miura/lc/array/DutchNationalFlag.kt
@@ -0,0 +1,10 @@
+package ar.com.miura.lc.array
+
+/**
+ * https://leetcode.com/problems/sort-colors/
+ */
+class DutchNationalFlag {
+    fun sortColors(nums: IntArray): Unit {
+        nums.sort()
+    }
+}
