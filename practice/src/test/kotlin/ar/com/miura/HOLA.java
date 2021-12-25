f1246aa3e8971ae3430415af46a07be3df3d75e3
agustinmiura <miura.agustin@gmail.com>
2021-12-25 14:03:23 -0300

    Added problems

---
+++ practice/src/test/kotlin/ar/com/miura/HOLA.java
@@ -0,0 +1,11 @@
+package ar.com.miura;
+
+public class HOLA {
+
+    public static void main(String[] args) {
+        String email = "name@sdfdsf.com";
+        String[] splitted = email.split("@");
+        email.indexOf('a');
+        String stop = "stop";
+    }
+}
