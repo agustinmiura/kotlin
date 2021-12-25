400072a64684d7c8bbfc8529e102f890072b88a3
agustinmiura <miura.agustin@gmail.com>
2021-12-25 11:44:43 -0300

    Added problems

---
+++ practice/src/main/kotlin/java/ar/com/miura/matrix/FlipAnImage.java
@@ -0,0 +1,46 @@
+package java.ar.com.miura.matrix;
+
+public class FlipAnImage {
+    public int[][] flipAndInvertImage(int[][] image) {
+
+        int rowQty = image.length;
+        for(int i=0;i<rowQty;i++) {
+            flipRow(image,i);
+        }
+
+        for(int i=0;i<rowQty;i++) {
+            invertRow(image, i);
+        }
+
+        return image;
+    }
+
+    private void flipRow(int[][] image, int rowIndex) {
+        int[] row = image[rowIndex];
+
+        int i =0;
+        int j = row.length-1;
+        while(i<j) {
+            int tempI = image[rowIndex][i];
+            image[rowIndex][i] = image[rowIndex][j];
+            image[rowIndex][j] = tempI;
+            i++;
+            j--;
+        }
+    }
+
+    private void invertRow(int[][] image, int rowIndex) {
+        int[] row = image[rowIndex];
+        for(int j=0;j<row.length;j++) {
+            image[rowIndex][j] = reverse(image[rowIndex][j]);
+        }
+    }
+
+    private int reverse(int number) {
+        if (number==1) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
---
+++ practice/src/main/kotlin/java/ar/com/miura/tree/LeafSimilarTree.java
@@ -0,0 +1,33 @@
+package java.ar.com.miura.tree;
+
+public class LeafSimilarTree {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        java.util.List<Integer> root1Leafs = new java.util.ArrayList<Integer>();
+        java.util.List<Integer> root2Leafs = new java.util.ArrayList<Integer>();
+
+        getLeaves(root1, root1Leafs);
+        getLeaves(root2, root2Leafs);
+
+        if (root1Leafs.size()!=root2Leafs.size()) {
+            return false;
+        } else {
+            return root1Leafs.equals(root2Leafs);
+        }
+
+    }
+
+    private void getLeaves(TreeNode node, java.util.List<Integer> leaves) {
+        if (node==null) {
+            return;
+        }
+        if (node.left!=null) {
+            getLeaves(node.left, leaves);
+        }
+        if (node.left==null && node.right==null) {
+            leaves.add(node.val);
+        }
+        if (node.right!=null) {
+            getLeaves(node.right, leaves);
+        }
+    }
+}
