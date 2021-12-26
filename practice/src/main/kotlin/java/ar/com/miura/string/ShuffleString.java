d610286d5584e9eff946a4af9cf0c8ab7b63566e
agustinmiura <miura.agustin@gmail.com>
2021-12-26 15:41:29 -0300

    Added problem

---
+++ practice/src/main/kotlin/java/ar/com/miura/string/ShuffleString.java
@@ -0,0 +1,15 @@
+package java.ar.com.miura.string;
+
+public class ShuffleString {
+    public String restoreString(String s, int[] indices) {
+
+        char[] cloned = new char[indices.length];
+        for(int i=0;i<indices.length;i++) {
+            int index = indices[i];
+            char tempChar = s.charAt(i);
+            cloned[index] = tempChar;
+        }
+
+        return new String(cloned);
+    }
+}
