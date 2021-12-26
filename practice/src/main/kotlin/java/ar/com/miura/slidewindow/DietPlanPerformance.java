4ba6b271ada639c9ba4621dca209f56c16604b6a
agustinmiura <miura.agustin@gmail.com>
2021-12-26 20:02:19 -0300

    Added problem

---
+++ practice/src/main/kotlin/java/ar/com/miura/slidewindow/DietPlanPerformance.java
@@ -0,0 +1,25 @@
+package java.ar.com.miura.slidewindow;
+
+public class DietPlanPerformance {
+    public int dietPlanPerformance(int[] calories, int k, int lower, int upper) {
+
+        int points = 0;
+        int size = calories.length;
+
+        for(int i=0;i<size-k+1;i++) {
+
+            int sum = 0;
+            for(int j=0; j<k && i+j<size ;j++) {
+                sum += calories[i+j];
+            }
+            if (sum<lower) {
+                points = points - 1;
+            } else if (sum>upper) {
+                points++;
+            }
+
+        }
+
+        return points;
+    }
+}
