d56bd1ad2a3f79c6edc64093de94bae07f8a6597
agustinmiura <miura.agustin@gmail.com>
2022-01-03 23:27:11 -0300

    Added problem

--- practice/src/main/kotlin/ar/com/miura/jvm/string/Palindrome.java
+++ practice/src/main/kotlin/ar/com/miura/jvm/string/Palindrome.java
@@ -1,46 +1,32 @@
 package ar.com.miura.jvm.string;
 
 public class Palindrome {
-    /**
-     If you do the brute force way you would generate a lot more strings than this method looks at.
-     which is set of all subsets ( rather substrings) -
-     E(sigma) (n-i) as i runs from 1 to n-1 = n-squared + n(n+1)/2 - O(n-squared) complexity.
-     This problem can be done using DP with n-squared complexity as shown above by [@GraceMeng](https://leetcode.com/GraceMeng)
-     with a few comments below by me based on that
-     **/
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() <= 1)
-            return s;
-        int len = s.length();
-        // dp[i][j] stands for status of a substring starting at i and ending at j incl j
-        boolean[][] dp = new boolean[len][len];
+    public static String longestPalindrome(String s) {
 
-        // initialize all the 1 character palins
-        for(int i = 0; i < len; i++)
-            dp[i][i] = true;
+        int n = s.length();
+        String res = null;
+        int palindromeStartsAt = 0, maxLen = 0;
 
-        // to compute dp[i][j] we need dp[i+1][j-1]
-        // so the i - outer loop needs to go from higher to lower
-        // and the j - inner loop needs to go from lower to higher
-        int maxLen = 0, maxSta = 0, maxEnd = 0;
-        for(int i = len; i >= 0; i--){
-            // dist of 0 - already covered by initialization
-            for(int dist = 1; dist < len - i; dist++){
-                int j = i + dist;
-                // we are ready to compute dp [i] [j]
-                if (dist == 1 && s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = true;
-                } else if (s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = dp[i+1][j-1];
-                }
-                // if true
-                if (dp[i][j] && maxLen < (j-i+1)){
-                    maxLen = j - i + 1;
-                    maxSta = i;
-                    maxEnd = j;
+        boolean[][] dp = new boolean[n][n];
+        // dp[i][j] indicates whether substring s starting at index i and ending at j is palindrome
+
+        for(int i = n-1; i >= 0; i--) { // keep increasing the possible palindrome string
+            for(int j = i; j < n; j++) { // find the max palindrome within this window of (i,j)
+
+                //check if substring between (i,j) is palindrome
+                dp[i][j] = (s.charAt(i) == s.charAt(j)) // chars at i and j should match
+                        &&
+                        ( j-i <= 2  // if window is less than or equal to 3, just end chars should match
+                                || dp[i+1][j-1]  ); // if window is > 3, substring (i+1, j-1) should be palindrome too
+
+                //update max palindrome string
+                if(dp[i][j] && (j-i+1 > maxLen)) {
+                    palindromeStartsAt = i;
+                    maxLen = j-i+1;
                 }
             }
         }
-        return s.substring(maxSta,maxEnd+1);
+        return s.substring(palindromeStartsAt, palindromeStartsAt+maxLen);
+
     }
 }
--- practice/src/test/kotlin/ar/com/miura/jvm/string/dp/PalindromeTestCase.java
+++ practice/src/test/kotlin/ar/com/miura/jvm/string/dp/PalindromeTestCase.java
@@ -6,7 +6,7 @@ public class PalindromeTestCase {
 
     @org.junit.jupiter.api.Test
     public void shouldFind() {
-        org.junit.jupiter.api.Assertions.assertEquals(3, new ar.com.miura.jvm.string.Palindrome().longestPalindrome("babad"));
+        org.junit.jupiter.api.Assertions.assertEquals("aba", new ar.com.miura.jvm.string.Palindrome().longestPalindrome("babad"));
     }
 
 }
