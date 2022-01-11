361845895cc3127e60ae0677beba4b9e27c81fbb
agustinmiura <miura.agustin@gmail.com>
2022-01-10 21:25:09 -0300

    Solved permutations

---
+++ practice/src/main/kotlin/ar/com/miura/jvm/permutations/basic/Permutations.java
@@ -0,0 +1,36 @@
+package ar.com.miura.jvm.permutations.basic;
+
+public class Permutations {
+    public List<java.util.List<Integer>> permute(int[] nums) {
+        List<java.util.List<Integer>> permutations = new ArrayList<java.util.List<Integer>>();
+        permutations(nums, 0, permutations);
+        return permutations;
+    }
+
+    private void permutations(int[] nums, int i, List<java.util.List<Integer>> permutations) {
+
+        int size = nums.length;
+        if (i==size-1) {
+            permutations.add(convert(nums));
+        } else {
+            for(int j=i;j<size;j++) {
+                swap(i,j, nums);
+                permutations(nums, i+1, permutations);
+                swap(j,i, nums);
+            }
+        }
+    }
+
+    private void swap(int i,int j, int[] nums) {
+        int tempI = nums[i];
+        int tempJ = nums[j];
+        nums[i] = tempJ;
+        nums[j] = tempI;
+    }
+
+    private java.util.List<Integer> convert(int[] numbers) {
+        return java.util.Arrays.asList(
+                java.util.Arrays.stream(numbers).boxed().toArray(Integer[]::new)
+        );
+    }
+}
