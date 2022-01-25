71d471d4fc7edfc41138be08852304f6e4793046
agustinmiura <miura.agustin@gmail.com>
2022-01-25 19:50:13 -0300

    Added first unique element

--- practice/src/main/kotlin/ar/com/miura/jvm/queue/FirstUniqueElement.java
+++ practice/src/main/kotlin/ar/com/miura/jvm/queue/FirstUniqueElement.java
@@ -1,4 +1,46 @@
 package ar.com.miura.jvm.queue;
 
 public class FirstUniqueElement {
+    
+    private Queue<Integer> queue = new LinkedList<Integer>();
+
+    private Map<Integer, Boolean> uniqueMap = new HashMap<>();
+
+    public FirstUnique(int[] nums) {
+        for(int num:nums) {
+            addNumber(num);
+        }
+    }
+
+    public int showFirstUnique() {
+
+        while( !queue.isEmpty() && !uniqueMap.getOrDefault(queue.peek(), false) ) {
+            queue.remove();
+        }
+
+        if (!queue.isEmpty()) {
+            return queue.peek();
+        } else {
+            return -1;
+        }
+
+
+    }
+
+    public void add(int value) {
+        addNumber(value);
+    }
+
+    private void addNumber(int number) {
+
+        if (!uniqueMap.containsKey(number)) {
+            uniqueMap.put(number ,true);
+            queue.add(number);
+
+        } else {
+            uniqueMap.put(number, false);
+
+        }
+
+    }
 }
