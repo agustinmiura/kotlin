0ede390716e3b4248355991267c2c0e67de50089
A R M <1576386-agustinmiura@users.noreply.gitlab.com>
2022-02-01 01:54:17 +0000

    feature/problem

---
+++ practice/src/main/kotlin/ar/com/miura/jvm/hashtable/NumberPairsInterchangeableRectangles.java
@@ -0,0 +1,15 @@
+package ar.com.miura.jvm.hashtable;
+
+public class NumberPairsInterchangeableRectangles {
+    public long interchangeableRectangles(int[][] rectangles) {
+        long count = 0;
+        java.util.Map<Double, Long> map = new java.util.HashMap<>();
+        for(int[] pair:rectangles) {
+            double ratio = (pair[0]*1.0) / (pair[1]*1.0);
+            long qty = map.getOrDefault(ratio, 0L);
+            count = count + qty;
+            map.put(ratio, map.getOrDefault(ratio, 0L)+1);
+        }
+        return count;
+    }
+}
