47dc51eec5b4bb9147293693e683ccd440474e61
agustinmiura <miura.agustin@gmail.com>
2022-04-03 22:03:46 -0300

    Added a problem

---
+++ practice/src/main/kotlin/ar/com/miura/jvm/math/RandomPickWithWeight.java
@@ -0,0 +1,29 @@
+package ar.com.miura.jvm.math;
+
+public class RandomPickWithWeight {
+    private int total = 0;
+    private int[] prefixSum;
+
+    public Solution(int[] w) {
+
+        this.prefixSum = new int[w.length];
+
+        for(int i=0;i<w.length;i++) {
+            total += w[i];
+            this.prefixSum[i] = total;
+        }
+
+    }
+
+    public int pickIndex() {
+        Random random = new Random();
+        int target = random.nextInt(total);
+
+        for(int i=0;i<prefixSum.length;i++) {
+            if (target<this.prefixSum[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
